315dfa37d3fa434573b44d37984c7ed9503e2126
Łukasz Pietrek <lpietrek@gmail.com>
2023-03-16 19:09:03 +0100

    Day 3 :: second solution

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-fn run(s: &str) -> i32 {
+fn first_solution(s: &str) -> i32 {
   let mut i = 0;
   let letters: HashMap<char, i32> = HashMap::from_iter(
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().map(|letter| {
@@ -24,10 +24,37 @@ fn run(s: &str) -> i32 {
     .sum()
 }
 
+fn second_solution(s: &str) -> i32 {
+  let mut i = 0;
+  let letters: HashMap<char, i32> = HashMap::from_iter(
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().map(|letter| {
+      i += 1;
+      (letter, i)
+    })
+  );
+  s.lines()
+    .map(|line| line.split_at(line.len() / 2))
+    .map(|(left, right)| left.chars().find(|lc| right.chars().any(|rc| lc == &rc)))
+    .map(|letter| {
+      match letter {
+        Some(letter) => letters.get(&letter),
+        None => Some(&0),
+      }
+    })
+    .map(|num| {
+      match num {
+        Some(num) => num,
+        None => &0,
+      }
+    })
+    .sum()
+}
+
 fn main() -> color_eyre::Result<()> {
   color_eyre::install()?;
 
-  test("first_solution", run);
+  test("first_solution", first_solution);
+  test("second_solution", second_solution);
 
   Ok(())
 }
